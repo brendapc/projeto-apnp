3f6159271f0c8207524594a21e824650fa8bb116
Brenda Camara <brendacamara@outlook.com>
2021-07-31 18:26:40 -0300

    feat: add new route

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -3,25 +3,32 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res)=>{
-    const sql = 'SELECT * FROM passagens'
+    const sql = 'SELECT * FROM tickets'
     db.query(sql, (err, result)=>{
         if (err) throw err
         res.status(200).json(result)
     })
 })
 
-router.get('/:destination', (req, res) => {
-    const sql = 'SELECT * FROM passagens WHERE loc_chegada = ?'
+router.get('/destinations/:destination', (req, res) => {
+    const sql = 'SELECT * FROM tickets WHERE loc_chegada = ?'
     db.query(sql, [ req.params.destination ], (err, result)=>{
         if(err) throw err
         res.status(200).json(result)
     })
 })
 
+router.get('/destinations', (req, res) => {
+    const sql = 'select * from tickets group by loc_chegada'
+    db.query(sql, (err, result)=>{
+        if(err) throw err
+        res.status(200).json(result)
+    })
+})
+
 router.get('/user/:id', (req, res )=>{
-    console.log(req.params.id)
     const id = req.params.id
-    const sql = 'SELECT passagens.numero, passagens.loc_chegada, usuarios.name, usuarios.id  FROM passagens, usuarios WHERE usuarios.id = passagens.id_user AND passagens.id_user = ?';
+    const sql = 'SELECT tickets.numero, tickets.loc_chegada, usuarios.name, usuarios.id  FROM tickets, usuarios WHERE usuarios.id = tickets.id_user AND tickets.id_user = ?';
     db.query(sql, id , (err, result)=>{
         if(err) throw err
         res.status(200).json(result)
@@ -29,7 +36,7 @@ router.get('/user/:id', (req, res )=>{
 })
 
 router.post('/', (req, res)=>{
-    const sql = 'INSERT INTO passagens VALUES (null, ?,?,?,?,?,?,?)'
+    const sql = 'INSERT INTO tickets VALUES (?,?,?,?,?,?,?)'
     db.query(sql, [req.body.numero, req.body.cia, req.body.loc_partida, req.body.loc_chegada, req.body.data_partida, req.body.data_chegada, req.body.preco ],
         (err, result)=>{
             if(err) throw err
@@ -38,7 +45,7 @@ router.post('/', (req, res)=>{
 })
 
 router.put('/:id', (req, res)=>{
-    const sql = 'UPDATE passagens SET numero = ?, cia = ?, loc_partida = ?, loc_chegada = ?, data_partida = ?, data_chegada = ?, preco = ?  WHERE id = ?'
+    const sql = 'UPDATE tickets SET numero = ?, cia = ?, loc_partida = ?, loc_chegada = ?, data_partida = ?, data_chegada = ?, preco = ?  WHERE id = ?'
     db.query(sql, [ req.body.numero, req.body.cia, req.body.loc_partida, req.body.loc_chegada, req.body.data_partida, req.body.data_chegada, req.body.preco, req.params.id ], 
         (err, result)=>{
             if(err) throw err
@@ -47,7 +54,7 @@ router.put('/:id', (req, res)=>{
 })
 
 router.delete('/:id', (req, res)=>{
-    const sql = 'DELETE FROM passagens WHERE id = ?'
+    const sql = 'DELETE FROM tickets WHERE id = ?'
     db.query(sql, [ req.params.id ], (err, result)=>{
         if(err) throw err
         res.status(200).json(result)
